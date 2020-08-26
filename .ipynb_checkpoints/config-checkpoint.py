2302734d33d50ca44afda83185e13bf3ae084892
Sabrina Wang <sawang@viya.globalhls.sashq-d.openstack.sas.com>
2020-08-26 17:18:49 -0400

    commit again

---
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -0,0 +1,10 @@
+# username
+username = 'sawang'
+password = "Orion123"
+# Reference the following blog to configuring secrets
+# https://blogs.sas.com/content/sgf/2019/01/25/authentication-to-sas-viya/
+oAuthCliendId = 'wang'
+oAuthClientSecret = 'wang'
+# build full API URL
+# seperate .py program
+base_url = 'http://viya.globalhls.sashq-d.openstack.sas.com/'
