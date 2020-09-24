7f4b5cc9cebfeeb76526fb0ceee3774dd00403cc
sabrinawang0118 <32717479+sabrinawang0118@users.noreply.github.com>
2020-09-24 09:31:21 -0400

    Update runSAS.py
    simplified code

--- runSAS.py
+++ runSAS.py
@@ -1,21 +1,56 @@
 #import requests and json modules
-import main
+import requests as request
+import json
+import time
+import config
 
-#Authenticate and obtain access token: 
-oAuthAccessToken= main.token()
+#import config variables
+base_url =config.base_url
+job_url =config.job_url
+username =config.username
+password = config.password
+oAuthCliendId =config.oAuthCliendId
+oAuthClientSecret =config.oAuthClientSecret
+# macro ='MSRP'
 
-# Select job to run; change to your jobname in viya 
-jobName = "yourJobName"
+#generate token
+url = base_url + '/SASLogon/oauth/token'
 
-# pass in macro
-# Example: MSRP, Invoice, EngineSize, Cylinders
-macro = "yourMacrovariable"
+# pass headers to API
+headers = { 
+    'content-type': 'application/x-www-form-urlencoded' 
+    }
+payload = 'grant_type=password&username=' + username + '&password=' + password
+r = request.post(url, payload, headers=headers, auth=(oAuthCliendId, oAuthClientSecret))
+responseObj = json.loads(r.text)
+oAuthAccessToken = responseObj['access_token']
 
-# run sas9 code
-response = main.runCode(jobName, macro)
+# submit job
+url= job_url +'&_action=json&_resultfile=*&_omittextlog=false'
+# url= job_url +'&param='+macro + '&_action=json&_resultfile=*&_omittextlog=false'
+headers = {
+  'Authorization': 'bearer ' + oAuthAccessToken,
+  'Content-Type': 'application/vnd.sas.job.execution.job.request',
+  'Accept': 'application/vnd.sas.job.execution.job'
+}
 
-#generate output.html for easy viewing
-main.output_html(response)
+r = request.post(url, headers = headers).json()
+print(r)
 
-#save SAS log into log.html for easy viewing
-main.log_html(response)
+#construct url to retrieve log and output table
+log_uri= base_url+ r[1]['href']
+output_uri = base_url +  r[2]['href']
+
+#generate log file
+log_str = request.get(log_uri, headers = headers).text
+
+filename = "Log_"+time.strftime("%Y%m%d-%H%M%S")+".txt"
+with open(filename, "w") as log:
+    log.write(log_str)
+
+
+#generate output table(s) file
+output_str = request.get(output_uri, headers = headers).text
+filename ="Output_"+time.strftime("%Y%m%d-%H%M%S")+".html"
+with open(filename, "w") as output_html:
+    output_html.write(output_str)
